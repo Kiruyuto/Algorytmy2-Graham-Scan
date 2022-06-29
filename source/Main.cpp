4b45c7ac64036885695ae18d4ade33729605f5f8
Hubert@Home <hubba2089@gmail.com>
2022-06-29 14:55:45 +0200

    Variable names change

--- source/Main.cpp
+++ source/Main.cpp
@@ -4,45 +4,45 @@
 #include <vector>
 #include <chrono>
 #include<sstream>  
-#include <iomanip> //<== setprecision()
+// #include <iomanip> //<== setprecision()
 using namespace std::chrono;
 
-// Klasa pojedynczego puinktu
-class punkt {
+// Represents single point in 2D space
+class point {
 public:
     double x, y;
     int index;
-    bool operator > (punkt p) {    //Lowest Y
+    bool operator > (point p) {    //Lowest Y
         if (y != p.y) { return y < p.y; }
         return x < p.x;
     };
 };
 
-// Wczytaj punkty z pliku
-std::vector<punkt> wczytaj(std::string filePath) {
-    std::vector<punkt> output;
-    punkt* punkty;
+// Load points from file
+std::vector<point> load(std::string filePath) {
+    std::vector<point> output;
+    point* points;
     std::ifstream plik;
     plik.open(filePath);
-    int iloscPunktow = 0;
+    int pointsCount = 0;
     if (plik.is_open()) {
-        plik >> iloscPunktow;
-        std::cout << "Ilosc punktow w danym pliku: " << iloscPunktow << std::endl;
-        punkty = new punkt[iloscPunktow];
-        for (int i = 0; i < iloscPunktow; i++) {
-            plik >> punkty[i].x >> punkty[i].y;
-            punkty[i].index = i;
+        plik >> pointsCount;
+        std::cout << "Ilosc punktow w danym pliku: " << pointsCount << std::endl;
+        points = new point[pointsCount];
+        for (int i = 0; i < pointsCount; i++) {
+            plik >> points[i].x >> points[i].y;
+            points[i].index = i;
         };
         plik.close();
-        for (int i = 0; i < iloscPunktow; i++) {
-            output.push_back(punkty[i]);
+        for (int i = 0; i < pointsCount; i++) {
+            output.push_back(points[i]);
         };
     };
     return output;
 };
 
-// Skret 3 punktow
-int CounterClockWise(punkt p1, punkt p2, punkt p3) {
+// 3 points turn
+int counterClockWise(point p1, point p2, point p3) {
     double ar = (p2.x - p1.x) * (p3.y - p1.y) - (p2.y - p1.y) * (p3.x - p1.x);
     if (ar < 0) return 1; //Clockwise
     if (ar > 0) return -1; //CounterClockWise
@@ -50,13 +50,13 @@ int CounterClockWise(punkt p1, punkt p2, punkt p3) {
 };
 
 // sqr dist
-double furtherAway(punkt p1, punkt p2) {
+double furtherAway(point p1, point p2) {
     return (p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y);
 };
 
-// Sortuj punkty
-void sortPoints(std::vector<punkt>& v1) {
-    //Wyszukuje lowestLeft
+// Sort points
+void sortPoints(std::vector<point>& v1) {
+    // Find lowestLeft
     double lowestLeft = v1[0].y;
     int min = 0;
     for (int i = 1; i < v1.size(); i++) {
@@ -67,43 +67,42 @@ void sortPoints(std::vector<punkt>& v1) {
         };
     };
     std::swap(v1[0], v1[min]);
-    punkt p0 = v1[0];
+    point p0 = v1[0];
     //cout << "\nNajnizszy lewy dolny: " << endl;
     //cout << "(" << p0.x << ", " << p0.y << ")" << endl;
-    auto compare = [p0](punkt& p1_2, punkt& p2_2) {
-        int returned = CounterClockWise(p0, p1_2, p2_2);
+    auto compare = [p0](point& p1_2, point& p2_2) {
+        int returned = counterClockWise(p0, p1_2, p2_2);
         if (returned == 0) { return  furtherAway(p0, p1_2) < furtherAway(p0, p2_2); }
         else { return returned == -1; };
     };
     sort(v1.begin() + 1, v1.end(), compare);
 };
 
-std::vector<punkt> grahamScan(std::vector<punkt> v1) {
-    std::vector<punkt> punkty;
+std::vector<point> grahamScan(std::vector<point> v1) {
+    std::vector<point> points;
     int rozmiarV1 = v1.size();
-    //Case dla malych tablic
-    if (rozmiarV1 <= 2) { // <=2 punkty = nie mozna utworzyc powloki
+    if (rozmiarV1 <= 2) { // can't make convex hull with <= 2 points
         return v1;
     };
-    punkty.push_back(v1[0]);
-    punkty.push_back(v1[1]);
-    punkty.push_back(v1[2]);
-    for (int i = punkty.size(); i < v1.size(); i++) {
-        auto ps = punkty.size();
-        while (punkty.size() > 1 && CounterClockWise(punkty[ps - 2], punkty[ps - 1], v1[i]) != -1) {
-            punkty.pop_back();
-            ps = punkty.size();
+    points.push_back(v1[0]);
+    points.push_back(v1[1]);
+    points.push_back(v1[2]);
+    for (int i = points.size(); i < v1.size(); i++) {
+        auto ps = points.size();
+        while (points.size() > 1 && counterClockWise(points[ps - 2], points[ps - 1], v1[i]) != -1) {
+            points.pop_back();
+            ps = points.size();
         };
-        punkty.push_back(v1[i]);
+        points.push_back(v1[i]);
     };
-    return punkty;
+    return points;
 };
 
-// Wyswietl rezultaty
-void display(std::vector<punkt> v1) {
+// Display results
+void display(std::vector<point> v1) {
     //std::cout << "Powloka wypukla: " << std::endl;
     int j = 0;
-    for (std::vector<punkt>::iterator::value_type const i : v1) {
+    for (std::vector<point>::iterator::value_type const i : v1) {
         std::cout << "[" << i.index << "] (" << i.x << ", " << i.y << ")" << std::endl;
         j++;
     }
@@ -111,22 +110,22 @@ void display(std::vector<punkt> v1) {
 };
 
 int main() {
-    std::vector<punkt> punkty;
+    std::vector<point> points;
     for (int i = 1; i <= 6; i++) {
         if (i != 6) {
-            punkty = wczytaj("../example-point-sets/points" + std::to_string(i) + ".txt");
+            points = load("../example-point-sets/points" + std::to_string(i) + ".txt");
         }
         else {
-            punkty = wczytaj("../example-point-sets/pointsTest.txt");
+            points = load("../example-point-sets/pointsTest.txt");
         };
         auto startSort = steady_clock::now();
-		sortPoints(punkty);
+		sortPoints(points);
         auto endSort = steady_clock::now();
         auto startMainLoop = steady_clock::now();
-        std::vector<punkt> powloka = grahamScan(punkty);
+        std::vector<point> convexHull = grahamScan(points);
         auto endMainLoop = steady_clock::now();
         std::cout << "Powloka wypukla\n[index] (wspolrzedna.x, wspolrzedna.y)" << std::endl;
-		display(powloka);
+		display(convexHull);
         std::cout << "\nCzas sortowania wynosi: " << duration_cast<microseconds>(endSort - startSort).count() << "us" << std::endl;
         std::cout << "Czas glownej petli wynosi: " << duration_cast<microseconds>(endMainLoop - startMainLoop).count() << "us" << std::endl;
         std::cout << "Czas laczny wynosi: ~" << duration_cast<microseconds>((endSort - startSort) + (endMainLoop - startMainLoop)).count() << "us" << std::endl;
